37f4ba4c5cac54b3ae09d95c50848df2cb19c0cf
mssalmansk <47742185+mssalmansk@users.noreply.github.com>
2021-10-28 08:34:15 +0530

    Create AccountApp.java

---
+++ day3/AccountApp.java
@@ -0,0 +1,12 @@
+package multipleaccount;
+
+public class accountapp {
+
+	public static void main(String[] args) {
+		SavingsAccount a1=new SavingsAccount();
+		a1.createaccount();
+		// TODO Auto-generated method stub
+
+	}
+
+}
